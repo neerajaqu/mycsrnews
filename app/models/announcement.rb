4c8f5029be3f02ed6946e98eef8bbade67c4f981
Jeff Reifman <Jeff@jeffmacbookpro.lan>
2010-02-16 18:26:38 -0800

    announcements model validation

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,2 +1,4 @@
 class Announcement < ActiveRecord::Base
+  validates_presence_of :title
+  validates_format_of :url, :with => /\Ahttp(s?):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/i, :message => "should look like a URL", :allow_blank => true
 end
