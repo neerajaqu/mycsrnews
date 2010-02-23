1c207078b84741f494077695c091430de38909db
Russell Branca <russell@beastly-leviathan.(none)>
2010-02-22 19:25:57 -0800

    Updating deleting blank users

--- db/migrate/20100103215300_delete_blank_users.rb
+++ db/migrate/20100103215300_delete_blank_users.rb
@@ -1,7 +1,6 @@
 class DeleteBlankUsers < ActiveRecord::Migration
   def self.up
     User.find(:all, :conditions => ["name = ''"]).each do |user|
-      user.profile.destroy if user.profile.present?
       user.destroy
     end
   end
