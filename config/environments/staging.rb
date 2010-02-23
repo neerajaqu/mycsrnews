fde9872a708b54afe352e162555736df85a581b2
Russell Branca <russell@beastly-leviathan.(none)>
2010-02-23 15:21:27 -0800

    Updating staging environment error reporting

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -8,8 +8,8 @@ config.cache_classes = false
 # Log error messages when you accidentally call methods on nil.
 config.whiny_nils = true
 
-# Show full error reports and disable caching
-config.action_controller.consider_all_requests_local = true
+# Don't show full error messages and disable caching
+config.action_controller.consider_all_requests_local = false
 config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
 
