66bd7b19f705990b02d06ab524c3b217f1845dbe
Jeff Reifman <Jeff@iMac24.local>
2010-03-02 14:03:23 -0800

    patch migration

--- db/migrate/20100302203538_contents_change_featured.rb
+++ db/migrate/20100302203538_contents_change_featured.rb
@@ -1,9 +1,9 @@
-class ContentChangeFeatured < ActiveRecord::Migration
+class ContentsChangeFeatured < ActiveRecord::Migration
   def self.up
     rename_column :contents, :isFeatured, :is_featured
   end
 
   def self.down
-    rename_column :contents,  :is_featured,:isFeatured
+    rename_column :contents,  :is_featured, :isFeatured
   end
 end
