e633548eabc9f7dd470d5cc9c48a6a9dc6d594cb
Jeff Reifman <Jeff@iMac24.local>
2010-03-17 00:28:36 -0700

    added welcome panel, removed comment reply for now, added template 4 to featured items, added comments to admin menu

---
+++ app/controllers/admin/comments_controller.rb
@@ -0,0 +1,72 @@
+class Admin::CommentsController < AdminController
+  skip_before_filter :admin_user_required
+
+  def index
+    render :partial => 'shared/admin/index_page', :layout => 'new_admin', :locals => {
+    	:items => Comment.paginate(:page => params[:page], :per_page => 20, :order => "created_at desc"),
+    	:model => Comment,
+    	:fields => [:comments, :postedByName, :created_at],
+    	:paginate => true
+    }
+  end
+
+  def new
+    render :partial => 'shared/admin/new_page', :layout => 'new_admin', :locals => {
+    	:model => Comment,
+    	:fields => [:comments, :postedByName, :created_at],
+    }
+  end
+
+  def edit
+    @Comment = Comment.find(params[:id])
+    render :partial => 'shared/admin/edit_page', :layout => 'new_admin', :locals => {
+    	:item => @Comment,
+    	:model => Comment,
+    	:fields => [:comments, :created_at],
+    }
+  end
+
+  def update
+    @Comment = Comment.find(params[:id])
+    if @Comment.update_attributes(params[:Comment])
+      flash[:success] = "Successfully updated your Comment."
+      redirect_to [:admin, @Comment]
+    else
+      flash[:error] = "Could not update your Comment as requested. Please try again."
+      render :edit
+    end
+  end
+
+  def show
+    render :partial => 'shared/admin/show_page', :layout => 'new_admin', :locals => {
+    	:item => Comment.find(params[:id]),
+    	:model => Comment,
+    	:fields => [:comments, :postedByName, :created_at],
+    }
+  end
+
+  def create
+    @Comment = Comment.new(params[:Comment])
+    if @Comment.save
+      flash[:success] = "Successfully created your new Comment!"
+      redirect_to [:admin, @Comment]
+    else
+      flash[:error] = "Could not create your Comment, please try again"
+      render :new
+    end
+  end
+
+  def destroy
+    @Comment = Comment.find(params[:id])
+    @Comment.destroy
+
+    redirect_to admin_Comments_path
+  end
+
+  private
+
+  def set_current_tab
+    @current_tab = 'Comments';
+  end
+
+end
