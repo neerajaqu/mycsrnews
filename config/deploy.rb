aa3b5d7754f5302ea4e07d1a4fe7c4ebaba4745c
Jeff Reifman <Jeff@iMac24.local>
2010-03-20 13:19:48 -0700

    added sites for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 set :default_stage, "n2_staging"
 set :stages, %w(n2_production n2_staging chewbranca_staging n2_charlotte n2_freep n2_kpcc n2_genomics n2_wkyc)
-#n2_sea n2_nc n2_buzz
+#n2_sea n2_nc n2_buzz n2_moreperfect n2_statepress
 require 'capistrano/ext/multistage'
 require 'eycap/recipes'
 
