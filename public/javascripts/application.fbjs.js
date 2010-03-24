b2127a66fae7834df33c2af978116c82b0f79350
Jeff Reifman <Jeff@iMac24.local>
2010-03-24 12:22:43 -0700

    improving jscript account menu

--- public/javascripts/application.fbjs.js
+++ public/javascripts/application.fbjs.js
@@ -50,7 +50,7 @@ $(function() {
   $('.account-toggle').click(function(event) {
   	event.preventDefault();  	
 	if ($(this).next().children().length==0) {
-		$(this).html("<img src=\""+SITE_URL+"/images/spinner.gif\" />");
+		$(this).next().html("<img style=\"float:right;\" src=\""+SITE_URL+"/images/spinner.gif\" />");
 		$(this).next().show(); 
 		$.update($(this).next(), SITE_URL+'/account_menu.fbjs');
 		$(this).next().show(); // force show on load
