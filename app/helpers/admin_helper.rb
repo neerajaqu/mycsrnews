89470927ae7af442d93e420b18007dbc6c68dcb8
Russell Branca <russell@beastly-leviathan.(none)>
2010-03-24 18:42:11 -0700

    Initial i18n db backend commit.

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -56,7 +56,7 @@ module AdminHelper
       html << "<tbody>"
       collection.each do |item|
         class_name = (item.moderatable? and item.blocked?) ? 'admin-blocked' : ''
-        html << "<tr class='#{class_name}'>"
+        html << "<tr class='#{class_name} #{cycle('odd', 'even')}'>"
         fields.each do |field|
           html << "<td>#{field_value item, field, options[:associations]}</td"
         end
