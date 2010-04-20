d30407a11445c2a7438ba358bcbd7f4a058a5611
Russell <russell@russell-laptop.(none)>
2010-04-20 16:44:16 -0700

    Updating deploy to work with new compass file names...

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ after("deploy:update_code") do
   %w{/config/unicorn.conf.rb /tmp/sockets /config/database.yml
     /config/facebooker.yml /config/application_settings.yml
     /config/application.god /config/newrelic.yml
-    /config/smtp.yml /config/menu.yml /config/compass.config}.each do |file|
+    /config/smtp.yml /config/menu.yml /config/compass.rb}.each do |file|
       run "ln -nfs #{shared_path}#{file} #{release_path}#{file}"
   end
 
