870d8c0703e1b5f349f158ec4d015b2bcd9c3aa4
Russell Branca <chewbranca@gmail.com>
2010-05-11 15:30:14 -0700

    Updating top items algorithm to work with utc time

--- lib/activerecord_model_extensions.rb
+++ lib/activerecord_model_extensions.rb
@@ -20,12 +20,13 @@ module Newscloud
         return self.all(:order => "created_at desc", :limit => limit) unless self.columns.select {|col| col.name == 'votes_tally'}
 
         table = self.name.tableize
+        now = Time.now.utc.strftime("%Y-%m-%d %H:%M:%S")
         # HACK ALERT
         # This will return an ordered set of results based on number of votes and time since posting
         if self.columns.select {|col| col.name == 'is_blocked'}
-          self.find_by_sql %{SELECT ((1 + (votes_tally * 2)) / (((UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(created_at)) / 3600) + 5)) AS item_score, #{table}.* FROM #{table} JOIN (SELECT ID FROM #{table} WHERE is_blocked = 0 ORDER BY created_at DESC LIMIT 100) AS sub_#{table} ON #{table}.id = sub_#{table}.id ORDER BY item_score DESC LIMIT #{limit};}
+          self.find_by_sql %{SELECT ((1 + (votes_tally * 2)) / (((UNIX_TIMESTAMP("#{now}") - UNIX_TIMESTAMP(created_at)) / 3600) + 5)) AS item_score, #{table}.* FROM #{table} JOIN (SELECT ID FROM #{table} WHERE is_blocked = 0 ORDER BY created_at DESC LIMIT 100) AS sub_#{table} ON #{table}.id = sub_#{table}.id ORDER BY item_score DESC LIMIT #{limit};}
         else
-          self.find_by_sql %{SELECT ((1 + (votes_tally * 2)) / (((UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(created_at)) / 3600) + 5)) AS item_score, #{table}.* FROM #{table} JOIN (SELECT ID FROM #{table} ORDER BY created_at DESC LIMIT 100) AS sub_#{table} ON #{table}.id = sub_#{table}.id ORDER BY item_score DESC LIMIT #{limit};}
+          self.find_by_sql %{SELECT ((1 + (votes_tally * 2)) / (((UNIX_TIMESTAMP("#{now}") - UNIX_TIMESTAMP(created_at)) / 3600) + 5)) AS item_score, #{table}.* FROM #{table} JOIN (SELECT ID FROM #{table} ORDER BY created_at DESC LIMIT 100) AS sub_#{table} ON #{table}.id = sub_#{table}.id ORDER BY item_score DESC LIMIT #{limit};}
         end
         #self.find_by_sql %{SELECT ((1 + (votes_tally * 2)) / (((UNIX_TIMESTAMP("2010-03-23 14:20:24") - UNIX_TIMESTAMP(created_at)) / 3600) + 5)) AS item_score, #{table}.* FROM #{table} JOIN (SELECT ID FROM #{table} ORDER BY created_at DESC LIMIT 100) AS sub_#{table} ON #{table}.id = sub_#{table}.id ORDER BY item_score DESC LIMIT #{limit};}
       end
@@ -65,6 +66,11 @@ module Newscloud
 
       def downvoteable?
         false
+      end
+
+      # model unique identifier
+      def muid
+        "#{self.class.name.underscore}_#{self.id}"
       end
 
       def item_title
