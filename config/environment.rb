d4eeec0fe9fac1f9c544ed95cd9d16bf7775f9d2
Justin Saul <justinsaul@gmail.com>
2010-05-20 10:34:47 -0700

    fancy date and time select for new events

--- config/environment.rb
+++ config/environment.rb
@@ -44,6 +44,7 @@ Rails::Initializer.run do |config|
   config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
   config.gem "compass"
   config.gem "compass-960-plugin", :lib => 'ninesixty'
+  config.gem "eostrom-zvent", :lib => 'zvent'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
