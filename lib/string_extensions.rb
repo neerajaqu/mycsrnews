a71840b41f448aaae4f2bf898981742327d8acb8
Russell Branca <chewbranca@gmail.com>
2010-07-02 10:27:08 -0700

    Quick sanitize update

--- lib/string_extensions.rb
+++ lib/string_extensions.rb
@@ -4,8 +4,8 @@ class String
   end
   
   def sanatize_standard()
-    raw = self.gsub!(/<!--(.*?)-->[\n]?/m, "")
-    ActionController::Base.helpers.sanitize(raw, :tags => %w(del, dd, h3, address, big, sub, tt, a, ul, h4, cite, dfn, h5, small, kbd, code,
+    self.gsub!(/<!--(.*?)-->[\n]?/m, "")
+    ActionController::Base.helpers.sanitize(self, :tags => %w(del, dd, h3, address, big, sub, tt, a, ul, h4, cite, dfn, h5, small, kbd, code,
        b, ins, h6, sup, pre, strong, blockquote, acronym, dt, br, p, div, samp,
        li, ol, var, em, h1, i, abbr, h2, span, hr), :attributes => %w(name, href, cite, class, title, src, height, datetime, alt, abbr, width))
   end
