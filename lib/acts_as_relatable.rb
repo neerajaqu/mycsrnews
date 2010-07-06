b49b4a9401b3435be971808199794421cecb9e52
Jeff Reifman <jeff@newscloud.com>
2010-07-06 11:02:01 -0700

    related items cleanup

--- lib/acts_as_relatable.rb
+++ lib/acts_as_relatable.rb
@@ -11,7 +11,7 @@ module Newscloud
       module ClassMethods
         def acts_as_relatable    
           has_many :related_items, :as => :relatable
-          named_scope :related_items, lambda { |*args| { :order => ["title asc"], :limit => (args.first || 12)} }
+          named_scope :list_items, lambda { |*args| { :order => ["title asc"], :limit => (args.first || 12)} }
           accepts_nested_attributes_for :related_items
           include Newscloud::Acts::Relatable::InstanceMethods
         end
