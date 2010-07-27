757fd273c70882357804c5b70ce2281fd1832c9c
Jeff Reifman <jeff@newscloud.com>
2010-07-27 15:39:39 -0700

    patch pfeed validations

--- app/models/pfeeds/user_triggered_dashboard_message.rb
+++ app/models/pfeeds/user_triggered_dashboard_message.rb
@@ -1,4 +1,6 @@
 class Pfeeds::UserTriggeredDashboardMessage < PfeedItem
+   validates_presence_of :participant, :originator
+  
    def pack_data(method_name,method_name_in_past_tense,returned_result,*args_supplied_to_method,&block_supplied_to_method) 
      super
    end
