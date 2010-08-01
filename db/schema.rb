a88f56fbe31e410ca740ca5eba3ea138feb564a1
Jeff Reifman <jeff@newscloud.com>
2010-08-01 13:16:59 -0700

    add read count to newswires

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100729204126) do
+ActiveRecord::Schema.define(:version => 20100730233038) do
 
   create_table "announcements", :force => true do |t|
     t.string   "prefix"
@@ -282,29 +282,6 @@ ActiveRecord::Schema.define(:version => 20100729204126) do
     t.datetime "updated_at"
   end
 
-  create_table "galleries", :force => true do |t|
-    t.string   "name"
-    t.string   "section"
-    t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "gallery_items", :force => true do |t|
-    t.integer  "user_id"
-    t.string   "title"
-    t.text     "caption"
-    t.integer  "votes_tally",    :default => 0
-    t.integer  "comments_count", :default => 0
-    t.boolean  "is_featured",    :default => false
-    t.datetime "featured_at"
-    t.integer  "flags_count",    :default => 0
-    t.boolean  "is_blocked",     :default => false
-    t.integer  "gallery_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "idea_boards", :force => true do |t|
     t.string   "name"
     t.string   "section"
@@ -399,6 +376,7 @@ ActiveRecord::Schema.define(:version => 20100729204126) do
     t.integer  "feed_id",                   :default => 0
     t.datetime "updated_at"
     t.boolean  "published",                 :default => false
+    t.integer  "read_count"
   end
 
   add_index "newswires", ["feed_id"], :name => "feedid"
@@ -654,20 +632,20 @@ ActiveRecord::Schema.define(:version => 20100729204126) do
   end
 
   create_table "user_profiles", :force => true do |t|
-    t.integer   "user_id",                     :limit => 8,                    :null => false
-    t.integer   "facebook_user_id",            :limit => 8, :default => 0
-    t.boolean   "isAppAuthorized",                          :default => false
-    t.datetime  "born_at"
-    t.timestamp "created_at",                                                  :null => false
-    t.datetime  "updated_at"
-    t.text      "bio"
-    t.integer   "referred_by_user_id",         :limit => 8, :default => 0
-    t.boolean   "comment_notifications",                    :default => false
-    t.boolean   "receive_email_notifications",              :default => true
-    t.boolean   "dont_ask_me_for_email",                    :default => false
-    t.datetime  "email_last_ask"
-    t.boolean   "dont_ask_me_invite_friends",               :default => false
-    t.datetime  "invite_last_ask"
+    t.integer  "user_id",                     :limit => 8,                    :null => false
+    t.integer  "facebook_user_id",            :limit => 8, :default => 0
+    t.boolean  "isAppAuthorized",                          :default => false
+    t.datetime "born_at"
+    t.datetime "created_at",                                                  :null => false
+    t.datetime "updated_at"
+    t.text     "bio"
+    t.integer  "referred_by_user_id",         :limit => 8, :default => 0
+    t.boolean  "comment_notifications",                    :default => false
+    t.boolean  "receive_email_notifications",              :default => true
+    t.boolean  "dont_ask_me_for_email",                    :default => false
+    t.datetime "email_last_ask"
+    t.boolean  "dont_ask_me_invite_friends",               :default => false
+    t.datetime "invite_last_ask"
   end
 
   add_index "user_profiles", ["user_id"], :name => "index_user_infos_on_user_id", :unique => true
