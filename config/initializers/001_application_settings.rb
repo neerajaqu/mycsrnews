3560e47fb708cad6eee8e26a11fe2613ae4a5c04
Russell <russell@russell-laptop.(none)>
2010-08-10 12:30:50 -0700

    Updating timezone settings

--- config/initializers/001_application_settings.rb
+++ config/initializers/001_application_settings.rb
@@ -4,3 +4,5 @@
 APPLICATION_CONFIGURATION_FILE_LOCATION = "#{RAILS_ROOT}/config/application_settings.yml"
 APP_CONFIG = YAML.load_file(APPLICATION_CONFIGURATION_FILE_LOCATION)[RAILS_ENV]
 ActionMailer::Base.default_url_options[:host] = APP_CONFIG['base_url'].sub(/^https?:\/\//,'')
+
+Time.zone = APP_CONFIG['time_zone'] || 'Pacific Time (US & Canada)'
