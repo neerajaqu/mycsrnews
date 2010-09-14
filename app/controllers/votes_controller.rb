01e4ed890ebd8c2419455f28d1e462eed520c43f
Russell Branca <chewbranca@gmail.com>
2010-09-14 11:55:54 -0700

    Updating vote messenger to explicitly set a caption and description to avoid duplication

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -9,7 +9,8 @@ class VotesController < ApplicationController
       error = (current_user and current_user.voted_for?(@voteable)) ? "You already voted" : false
       if !error and current_user and @voteable.present? and (vote = current_user.vote_for(@voteable))
       	image_url = (vote.voteable.respond_to?(:images) and vote.voteable.images.any?) ? @template.base_url(vote.voteable.images.first.url(:thumb)) : nil
-      	vote.async_vote_messenger polymorphic_path(@voteable.item_link, :only_path => false, :canvas => iframe_facebook_request?, :format => 'html'), image_url
+      	app_caption = t('app.facebook.vote_caption', :title => APP_CONFIG['site_title'])
+      	vote.async_vote_messenger polymorphic_path(@voteable.item_link, :only_path => false, :canvas => iframe_facebook_request?, :format => 'html'), app_caption, image_url
       	success = "Thanks for your vote!"
       	format.html { flash[:success] = success; redirect_to params[:return_to] || @voteable }
       	format.json { render :json => { :trigger_oauth => current_user.fb_oauth_desired?, :msg => "#{@voteable.votes_tally} likes" }.to_json }
--- app/workers/vote_messenger.rb
+++ app/workers/vote_messenger.rb
@@ -1,13 +1,13 @@
 class VoteMessenger
   @queue = :vote_messenger
 
-  def self.perform(vote_id, item_url, image_url)
+  def self.perform(vote_id, item_url, app_caption, image_url)
     vote = Vote.find(vote_id)
 
-    self.facebook_messenger vote, item_url, image_url
+    self.facebook_messenger vote, item_url, app_caption, image_url
   end
 
-  def self.facebook_messenger vote, item_url, image_url
+  def self.facebook_messenger vote, item_url, app_caption, image_url
     fb_client = Mogli::Client.new vote.voter.fb_oauth_key
     begin
       caption = ActionController::Base.helpers.strip_tags(vote.voteable.item_description)
@@ -19,7 +19,7 @@ class VoteMessenger
       end
     end
     caption = caption[0,250]
-    fb_client.post("#{vote.voter.fb_user_id}/feed", "Post", :message => "likes", :link => item_url, :name => vote.voteable.item_title, :caption => caption, :picture => image_url)
+    fb_client.post("#{vote.voter.fb_user_id}/feed", "Post", :message => "likes", :link => item_url, :name => vote.voteable.item_title, :caption => app_caption, :description => caption, :picture => image_url)
   end
     
 end
