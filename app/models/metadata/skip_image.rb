903748f9e765c542cfc05c8028805def3c235ac4
Jeff Reifman <jeff@newscloud.com>
2010-11-02 11:46:21 -0700

    remove validations from skip_image to allow partial string filtering

--- app/models/metadata/skip_image.rb
+++ app/models/metadata/skip_image.rb
@@ -2,10 +2,11 @@ class Metadata::SkipImage < Metadata
   after_save :remove_all_images
   named_scope :key_sub_type_name, lambda { |*args| { :conditions => ["key_sub_type = ? AND key_name = ?", args.first, args.second] } }
 
+  # taking these out to allow for partial strings
   # HACK:: emulate validate_presence_of
   # these are dynamicly created attributes so they don't exist for the model
-  validates_format_of :image_url, :with => /\Ahttp(s?):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/i, :message => "should look like a URL", :allow_blank => false
-  validates_format_of :image_url, :with => /\.(jpg|jpeg|gif|png)/, :message => "Background must be an image (jpg, jpeg, gif or png)"
+  # validates_format_of :image_url, :with => /\Ahttp(s?):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/i, :message => "should look like a URL", :allow_blank => false
+  # validates_format_of :image_url, :with => /\.(jpg|jpeg|gif|png)/, :message => "Background must be an image (jpg, jpeg, gif or png)"
 
   def self.get image_url, sub_type = nil
     self.find_skip_image(image_url, sub_type)
