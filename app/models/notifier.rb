6469d69780df0fb5ccf82ac1921c17935dba4832
Jeff Reifman <jeff@newscloud.com>
2010-11-17 15:32:50 -0800

    chg to metadata setting for contact_us

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -5,7 +5,7 @@ class Notifier < ActionMailer::Base
   def contact_us_message(message)
     subject       "Contact Us: #{message.subject}"
     from          message[:email]
-    recipients    APP_CONFIG['contact_us_recipients']
+    recipients    (Metadata::Setting.get_setting('contact_us').try(:value) || APP_CONFIG['contact_us_recipients'])
     sent_on       Time.now
     body          :message => message
     content_type  "text/html"
