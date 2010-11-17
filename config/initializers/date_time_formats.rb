03c625b012e341d1e5b28c112b95495254149a53
Eye Level <eyelevel@ileveloffice.homeip.net>
2010-11-17 13:37:55 -0800

    events revisions, minor layout fixes

--- config/initializers/date_time_formats.rb
+++ config/initializers/date_time_formats.rb
@@ -2,7 +2,7 @@ ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
   :us => '%m/%d/%y',
   :us_with_time => '%m/%d/%y, %l:%M %p',
   :short_with_time => '%B %e, %l:%M %p',
-  :short_day => '%e %B %Y',
+  :short_day => '%B %e, %Y',
   :long_day => '%A, %e %B %Y',
   :short_time => '%l:%M %p'
 )
