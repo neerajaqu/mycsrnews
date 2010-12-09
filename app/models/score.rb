cdc5924cc0255a2606a913148f7195c5c10109aa
Jeff Reifman <jeff@newscloud.com>
2010-12-09 13:09:11 -0800

    blocked users out of score list, auto_feature_mini taller, more lines, topic lists now show forum name, top discussions now look for older stories

--- app/models/score.rb
+++ app/models/score.rb
@@ -3,7 +3,7 @@ class Score < ActiveRecord::Base
   belongs_to :user
   belongs_to :scorable, :polymorphic => true
 
-  named_scope :for_user, lambda { |*args| { :conditions => ["user_id = ?", args.first], :order => ["created_at desc"] } }
+  named_scope :for_user, lambda { |*args| { :conditions => ["is_blocked = false and user_id = ?", args.first], :order => ["created_at desc"] } }
 
 
   def self.daily_scores limit = nil
