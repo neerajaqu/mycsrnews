01368924d6bc1b43bfcfa8ba749c9ac674ab472e
Jeff Reifman <jeff@newscloud.com>
2011-01-14 14:25:54 -0800

    update sweeper and sidebar prediction scores widget

--- app/sweepers/prediction_sweeper.rb
+++ app/sweepers/prediction_sweeper.rb
@@ -61,10 +61,12 @@ class PredictionSweeper < ActionController::Caching::Sweeper
 
   def self.expire_prediction_result_all prediction_result
     controller = ActionController::Base.new
-    ["#{prediction_result.cache_key}_voices"].each do |fragment|
+    ["#{prediction_result.cache_key}_voices","prediction_high_scores_html"].each do |fragment|
       controller.expire_fragment fragment
     end
-
+    ['', 'page_1_', 'page_2_'].each do |page|
+      expire_fragment "prediction_scores_list_#{page}html"
+    end
     PredictionSweeper.expire_prediction_question_all prediction_result.prediction_question
   end
 
