dfe53ae944398046dc94d67b672bf233c0db2c60
Russell <russell@russell-laptop.(none)>
2011-01-21 17:25:50 -0800

    Topic spec update

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -2,7 +2,11 @@ require 'spec_helper'
 
 describe Topic do
   it "should create a new instance given valid attributes" do
-    Factory.create(:topic)
+    pending "need to migrate logic from topics controller into topic model"
+    topic = Factory.create(:topic)
+    topic.posts.should_not be_empty
   end
+
+  it "should create the associated posts (need to migrate logic from topics controller into topic model)"
 
 end
