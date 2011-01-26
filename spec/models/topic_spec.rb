1d36338dd3f229dacb321f60a54490d625cece17
Russell <russell@russell-laptop.(none)>
2011-01-26 15:09:16 -0800

    Updating topic spec

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -8,5 +8,28 @@ describe Topic do
   end
 
   it "should create the associated posts (need to migrate logic from topics controller into topic model)"
+  it "should appropriately call comments_callback"
+  it "should set valid crumb parents"
+  it "should set a valid crumb link"
+
+  describe "#details" do
+    before(:each) do
+      @topic = Factory.create(:topic)
+      2.times { @topic.posts.push Factory.create(:comment, :commentable => @topic) }
+    end
+
+    it "should set a valid item description" do
+      @topic.item_description.should == @topic.posts.first.comments
+    end
+
+    it "should have a valid last post" do
+      @topic.last_post.should == @topic.posts.last
+    end
+
+    it "should set valid voices count" do
+      pending("Fix topic posts create")
+      @topic.voices_count.should == 3
+    end
+  end
 
 end
