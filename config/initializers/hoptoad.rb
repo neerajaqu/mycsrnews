197014268d5fd3999a09f9270e6d645cd39cb2cf
Jeff Reifman <jeff@newscloud.com>
2011-02-07 14:31:11 -0800

    update hoptoad move to settings to avoid needing seeding

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,5 +1,5 @@
-hoptoad_api_key = Metadata::Setting.find_setting('hoptoad_api_key').try(:value)
-if hoptoad_api_key
+hoptoad_api_key = ( Metadata::Setting.find_setting('hoptoad_api_key').try(:value) || APP_CONFIG['hoptoad_api_key'] )
+if hoptoad_api_key 
   HoptoadNotifier.configure do |config|
     config.api_key = hoptoad_api_key
   end
