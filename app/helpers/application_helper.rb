cd54c33594494a61669aca28166745b40f682377
Jeff Reifman <jeff@newscloud.com>
2011-02-09 10:57:06 -0800

    patch user_spec

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,4 @@
-Metadat# Methods added to this helper will be available to all templates in the application.
+# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   include TagsHelper
 
