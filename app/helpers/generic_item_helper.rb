fd5fb0a04c6206a71af96df51a38b90711782848
Jeff Reifman <jeff@newscloud.com>
2011-02-16 10:33:02 -0800

    adding widgets for d2

--- app/helpers/generic_item_helper.rb
+++ app/helpers/generic_item_helper.rb
@@ -5,6 +5,11 @@ module GenericItemHelper
     render :partial => "shared/items/#{klass}", :locals => { klass.to_sym => item }
   end
 
+  def d2_render_generic_item item
+    klass = item.class.name.underscore
+    render :partial => "shared/items/d2_#{klass}", :locals => { klass.to_sym => item }
+  end
+
   def render_generic_small_item item
     klass = item.class.name.underscore
     render :partial => "shared/items/small_#{klass}", :locals => { klass.to_sym => item }
