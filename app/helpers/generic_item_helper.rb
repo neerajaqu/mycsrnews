3adc6178ecf9ee5cb332fdb715e230cf891d3b23
Jeff Reifman <jeff@newscloud.com>
2011-02-16 14:30:04 -0800

    adding new d2 widgets

--- app/helpers/generic_item_helper.rb
+++ app/helpers/generic_item_helper.rb
@@ -7,7 +7,9 @@ module GenericItemHelper
 
   def d2_render_generic_item item
     klass = item.class.name.underscore
-    render :partial => "shared/items/d2_#{klass}", :locals => { klass.to_sym => item }
+    if klass == 'content' or klass == 'resource'
+      render :partial => "shared/items/d2_#{klass}", :locals => { klass.to_sym => item }
+    end
   end
 
   def render_generic_small_item item
