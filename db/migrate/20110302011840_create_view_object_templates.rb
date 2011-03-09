c9a880c6659ee272b26893bf8547f3d6dc581182
Russell Branca <chewbranca@gmail.com>
2011-03-09 13:00:24 -0800

    Adding pretty name to view object template migration

--- db/migrate/20110302011840_create_view_object_templates.rb
+++ db/migrate/20110302011840_create_view_object_templates.rb
@@ -3,12 +3,16 @@ class CreateViewObjectTemplates < ActiveRecord::Migration
     create_table :view_object_templates do |t|
       t.string :template
       t.string :name
+      t.string :pretty_name
 
       t.timestamps
     end
+
+    add_index :view_object_templates, :name
   end
 
   def self.down
+    remove_index :view_object_templates, :name
     drop_table :view_object_templates
   end
 end
